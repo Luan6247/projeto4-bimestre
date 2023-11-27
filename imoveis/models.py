a4af1ee585d9215daca1835c88faac93108f0d99
luan6247 <luantj150@gmail.com>
2023-11-27 08:35:42 -0300

    added spaces

--- imoveis/models.py
+++ imoveis/models.py
@@ -4,6 +4,7 @@ class Imovel(models.Model):
     proprietario = models.CharField(max_length=50)
     inquilino = models.CharField(max_length=50)
     ContratoAluguel = models.DateField()
+
 class Usuario(models.Model):
     #id_usuario = models.AutoField(primary_key=True)
     nome = models.TextField(max_length=255)
